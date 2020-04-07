d821989238ea36dcb92d2e84d6be338933827f4e
Doli Geetha Sai Kumari <62688828+Geetha63@users.noreply.github.com>
2020-04-07 17:14:17 +0530

    Folder update

---
+++ GuestRestrictionForGivenTeams/GuestRestricationForGivenTeams.ps1
@@ -0,0 +1,143 @@
+﻿#Keep tenant id, client id, client secret in info.json file run the script 
+#this script will take the input from current folder and create output in current folder  (keep the info.json file in same folder where you are running the script)
+#this script will change the Given Teams groups AllowToAddGuests value to False
+
+#creating token id
+$input = get-content info.json | ConvertFrom-Json
+$Client_Secret = $input.Client_Secret
+$client_Id = $input.client_Id
+$Tenantid = $input.Tenantid
+
+
+#Grant Adminconsent 
+$Grant= 'https://login.microsoftonline.com/common/adminconsent?client_id='
+$admin = '&state=12345&redirect_uri=https://localhost:1234'
+$Grantadmin = $Grant + $client_Id + $admin
+
+start $Grantadmin
+write-host "login with your tenant login detials to proceed further"
+
+$proceed = Read-host " Press Y to continue "
+if ($proceed -eq 'Y')
+{
+    write-host "Creating Access_Token"          
+              $ReqTokenBody = @{
+         Grant_Type    =  "client_credentials"
+        client_Id     = "$client_Id"
+        Client_Secret = "$Client_Secret"
+        Scope         = "https://graph.microsoft.com/.default"
+    } 
+
+    $loginurl = "https://login.microsoftonline.com/" + "$Tenantid" + "/oauth2/v2.0/token"
+    $Token = Invoke-RestMethod -Uri "$loginurl" -Method POST -Body $ReqTokenBody -ContentType "application/x-www-form-urlencoded"
+
+    $Header = @{
+        Authorization = "$($token.token_type) $($token.access_token)"
+    }
+
+ #Getting Team details
+    $TeamsList = Import-Csv "input.csv"
+    foreach($Teams in $TeamsList){
+  
+                        #$Teams.Teamsid
+                        #$Teams.AllowToAddGuests
+
+                        $uri = "https://graph.microsoft.com/v1.0/groups/" + $Teams.Teamsid
+                        $details = Invoke-RestMethod -Headers $Header -Uri $uri -Method Get -ContentType 'application/json'
+                        
+                        #Get group settings
+                        $settingsuri = "https://graph.microsoft.com/v1.0/groups/" + $Teams.Teamsid + "/settings"
+                        $groupsettings = Invoke-RestMethod -Headers $Header -Uri $settingsuri -Method get -ContentType 'application/json'
+                        $Groupvalue = $groupsettings.value
+        $body = '{
+  "displayName": "Group.Unified.Guest",
+  "templateId": "08d542b9-071f-4e16-94b0-74abb372e3d9",
+  "values": [
+    {
+      "name": "CustomBlockedWordsList",
+      "value": ""
+    },
+    {
+      "name": "EnableMSStandardBlockedWords",
+      "value": "False"
+    },
+    {
+      "name": "ClassificationDescriptions",
+      "value": ""
+    },
+    {
+      "name": "DefaultClassification",
+      "value": ""
+    },
+    {
+      "name": "PrefixSuffixNamingRequirement",
+      "value": ""
+    },
+    {
+      "name": "AllowGuestsToBeGroupOwner",
+      "value": "False"
+    },
+    {
+      "name": "AllowGuestsToAccessGroups",
+      "value": "False"
+    },
+    {
+      "name": "GuestUsageGuidelinesUrl",
+      "value": ""
+    },
+    {
+      "name": "GroupCreationAllowedGroupId",
+      "value": "62e90394-69f5-4237-9190-012177145e10"
+    },
+    {
+      "name": "AllowToAddGuests",
+      "value": "'+$Teams.AllowToAddGuests+'"
+    },
+    {
+      "name": "UsageGuidelinesUrl",
+      "value": ""
+    },
+    {
+      "name": "ClassificationList",
+      "value": ""
+    },
+    {
+      "name": "EnableGroupCreation",
+      "value": "True"
+    }
+  ]
+}'
+                        
+                          if($Groupvalue.id){
+                                               #if settings available update the settings
+                                               $settingsid = $Groupvalue.id
+                                               $patchuri = "https://graph.microsoft.com/v1.0/groups/" +$Teams.Teamsid+ "/settings/" + "$settingsid" 
+                                               $updatesettings = Invoke-RestMethod -Headers $Header -Uri $patchuri -Method Patch -Body $body -ContentType 'application/json'
+                                               write-host "settings has been updated for" $details.displayName
+                                              }
+                                        else{ 
+                                              #create settings template and apply
+                                              $createuri = "https://graph.microsoft.com/v1.0/groups/" +$Teams.Teamsid+ "/settings/"
+                                              $newsettings = Invoke-RestMethod -Headers $Header -Uri $createuri -Method Post -Body $body -ContentType 'application/json'
+                                              write-host "new settings has been created and applied to Team" $details.displayName
+                                              }
+
+                      $finaluri = "https://graph.microsoft.com/v1.0/groups/" +$Teams.Teamsid+ "/settings" 
+                      $final = Invoke-RestMethod -Headers $Header -Uri $finaluri -Method Get -ContentType 'application/json'
+                      $status = $final.value.values.value
+                      
+                      $Teamuri = "https://graph.microsoft.com/v1.0/groups/" + $Teams.Teamsid
+                      $Teamdetails = Invoke-RestMethod -Headers $Header -Uri $Teamuri -Method Get -ContentType 'application/json'
+                      
+                      write-host "exporting data for Team" $Teamdetails.displayName
+                      $file = New-Object psobject
+                      $file | add-member -MemberType NoteProperty -Name TeamsName $Teamdetails.displayName
+                      $file | add-member -MemberType NoteProperty -Name Teamsid $Teams.Teamsid
+                      $file | add-member -MemberType NoteProperty -Name AllowToAddGuests $status
+                      $file | export-csv output.csv -NoTypeInformation -Append
+                      } 
+          }
+ else 
+{
+    write-host "You need to login admin consent in order to continue... " 
+}
---
+++ GuestRestrictionForGivenTeams/README.md
@@ -0,0 +1,51 @@
+#### Description:
+
+This script will restrict the guest users in Teams by applying restriction for O365 Group.Script will take the input from current folder and create output in current folder (keep the info.json file in same folder where you are running the script)
+
+Powershell 3.0 or more should be there.
+
+Right-click Windows PowerShell, then select Run as Administrator. Click Yes at the UAC prompt.
+
+Prepare input.csv file in below format and keep it in current location where script is running 
+
+![Input]()
+
+  Keep the Below details in info.json file
+            "client_Id":  "please paste App provider application id here"
+            "Client_Secret":  "please paste App provided client secret here"
+            "Tenantid":  "please provide requester tenant id here"
+
+
+**Note**: Client id and client secret will be same for every tenant. Only tenant id must be changed to requester tenant id
+
+Run the script it will open webpage for grant permissions.
+
+Please login with your tenant _USER ID_ and _PASSWORD_.
+
+![Signin](https://github.com/Geetha63/MS-Teams-Scripts/blob/master/Images/Siginin.png)
+
+Once you are login it will shows the below image for Grant permissions for the app to perform the operations
+
+![GrantPermission](https://github.com/Geetha63/MS-Teams-Scripts/blob/master/Images/GrantPermissions.png)
+![GrantPermission](https://github.com/Geetha63/MS-Teams-Scripts/blob/master/Images/GrantPermissions2.png)
+
+**Click Accept**
+
+If you have provided the correct credentials it will give success status `admin_consent = True`
+
+![Admin Consent](https://github.com/Geetha63/MS-Teams-Scripts/blob/master/Images/AdminConsent.png)
+
+Now press Y to proceed further in script.
+
+Script will take the input from _input.csv_
+
+Once the script executed below output.csv file will generate.
+
+Sample Output:
+
+|TeamsName	| AllowToAddGuests |
+|-----------|------------------|
+|HR	        | FALSE            |
+|IT Team	| FALSE            |
+|Admin      | FALSE            |
+|Accounts   | FALSE            |
