d527cc7143730654db21820fc3f5848bfaf0a359
Doli Geetha Sai Kumari <62688828+Geetha63@users.noreply.github.com>
2020-07-23 11:12:14 +0530

    SharePoint storage

---
+++ SharePoint storage limit/sharepointStorageBasicScript.ps1
@@ -0,0 +1,13 @@
+﻿# site name = https://contoso.sharepoint.com/sites/HrTeam
+# StorageQuota = 20000 (input is in GB)
+# StorageQuotaWarningLevel = 19000 (input is in GB)]
+
+param(
+      [Parameter(Mandatory=$true)][System.String]$SiteName,
+      [Parameter(Mandatory=$true)][System.String]$StorageQuota,
+      [Parameter(Mandatory=$true)][System.String]$StorageQuotaWarningLevel    
+      )
+
+
+Set-SPOSite -Identity $SiteName -StorageQuota $StorageQuota -StorageQuotaWarningLevel $StorageQuotaWarningLevel -NoWait
+
