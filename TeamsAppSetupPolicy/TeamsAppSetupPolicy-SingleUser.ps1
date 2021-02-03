533c8ede4c49722d2c3c272e2716003e413817ef
Doli Geetha Sai Kumari <62688828+Geetha63@users.noreply.github.com>
2021-02-04 00:57:11 +0530

    Update TeamsAppSetupPolicy-SingleUser.ps1

--- TeamsAppSetupPolicy/TeamsAppSetupPolicy-SingleUser.ps1
+++ TeamsAppSetupPolicy/TeamsAppSetupPolicy-SingleUser.ps1
@@ -1,12 +1,35 @@
-﻿param(
-      [Parameter(Mandatory=$true)][System.String]$Policyname,
-      [Parameter(Mandatory=$true)][System.String]$user
-      )
-$credential = Get-credential
+$logfile = "C:\TeamsAppSetupPolicy-SingleUserlog_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+$start = [system.datetime]::Now 
+
+ If(Get-Module -ListAvailable -Name SkypeOnlineConnector) 
+ { 
+ Write-Host "SkypeOnlineConnector Already Installed" 
+ } 
+ else { 
+    try {
+         Write-Host "Installing SkypeOnlineConnector" 
+         Install-Module -Name SkypeOnlineConnector
+        }
+ catch{
+        $_.Exception.Message | out-file -Filepath $logfile -append
+ }
+ }
 Import-Module SkypeOnlineConnector
-$sfbSession = New-CsOnlineSession
-Import-PSSession $sfbSession
+$sfbSession = New-CsOnlineSession 
+Import-PSSession $sfbSession -AllowClobber
+
+$Policyname=Read-Host "Please enter PolicyName"
+$user=Read-Host "Please enter User Principal Name"
+try{
 
 Grant-CsTeamsAppsetupPolicy -policyname "$Policyname" -Identity  $user
 
-get-csonlineuser -Identity "$user" |ft TeamsappsetupPolicy
+get-csonlineuser -Identity "$user" |ft TeamsappsetupPolicy,UserPrincipalName
+}
+catch{
+     $_.Exception.Message | out-file -Filepath $logfile -append
+     }
+
+$end = [system.datetime]::Now
+$resultTime = $end - $start
+Write-Host "Execution took : $($resultTime.TotalSeconds) seconds." -ForegroundColor Cyan
