46d16610d914c6b1ba2d4cfebc73c05e1ba78d43
Doli Geetha Sai Kumari <62688828+Geetha63@users.noreply.github.com>
2021-02-04 09:54:28 +0530

    Update TeamsCreationRestrictionPolicy.ps1

--- Group or TeamsCreationRestrictionPolicy/TeamsCreationRestrictionPolicy.ps1
+++ Group or TeamsCreationRestrictionPolicy/TeamsCreationRestrictionPolicy.ps1
@@ -1,27 +1,39 @@
-﻿
-param(
-      [Parameter(Mandatory=$true)][System.String]$Groupname,
-      [Parameter(Mandatory=$true)][System.String]$AllowGroupCreation
-      )
+﻿$logfile = "C:\GroupTeamsCreationRestrictionPolicylog_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+ start
+$start = [system.datetime]::Now
+$Groupname = Read-host "Please provide group name"
+$AllowGroupCreation = Read-host "Please provide allow group creation yes or no"
+try{
 $conDetails = Connect-AzureAD
 $tenantDomain = $conDetails.TenantDomain
+}
+catch{
+$_.Exception.Message | out-file -Filepath $logfile -append
+}
 
 $settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
  if(!$settingsObjectID)
-    {
+    {try{
 	$template = Get-AzureADDirectorySettingTemplate | Where-object {$_.displayname -eq "group.unified"}
 	$settingsCopy = $template.CreateDirectorySetting()
 	New-AzureADDirectorySetting -DirectorySetting $settingsCopy
 	$settingsObjectID = (Get-AzureADDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+     } 
+     catch{
+$_.Exception.Message | out-file -Filepath $logfile -append
+}
      }
 
 	$settingsCopy = Get-AzureADDirectorySetting -Id $settingsObjectID
 	$settingsCopy["EnableGroupCreation"] = $AllowGroupCreation
 
  if($GroupName)
-   {
+   {try{
 	$settingsCopy["GroupCreationAllowedGroupId"] = (Get-AzureADGroup -SearchString $GroupName).objectid
     }
+    catch{
+$_.Exception.Message | out-file -Filepath $logfile -append
+}}
 
     Set-AzureADDirectorySetting -Id $settingsObjectID -DirectorySetting $settingsCopy
 
