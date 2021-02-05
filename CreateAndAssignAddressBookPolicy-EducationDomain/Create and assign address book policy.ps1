bdbd392ef69cc174a28a88f6aec6d61ce08cb9e5
Doli Geetha Sai Kumari <62688828+Geetha63@users.noreply.github.com>
2021-02-05 16:33:32 +0530

    Update Create and assign address book policy.ps1

--- CreateAndAssignAddressBookPolicy-EducationDomain/Create and assign address book policy.ps1
+++ CreateAndAssignAddressBookPolicy-EducationDomain/Create and assign address book policy.ps1
@@ -1,10 +1,27 @@
-﻿#Creating and assigning address policy
+$logfile = "C:\CreateAndAssignAddressbookPolicylog_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+$start = [system.datetime]::Now
+if(Get-Module -ListAvailable -Name ExchangeOnlineManagement) 
+ { 
+ Write-Host "ExchangeOnlineManagement Already Installed" 
+ } 
+ else { 
+ try { 
+Install-Module -Name ExchangeOnlineManagement
+ Write-Host "ExchangeOnlineManagement is installing"
+ }
+ catch{
+        $_.Exception.Message | out-file -Filepath $logfile -append
+ }
+ }
+ #Creating and assigning address policy
+try{
 $cred = Connect-ExchangeOnline
 $user=  Get-Mailbox -ResultSize unlimited
 $school = $user.CustomAttribute10 |  Sort-Object | Get-Unique
 $school=$school.Where({ $_ -ne "" })
 $school=$school.Trim()
-foreach ($school in $school){
+foreach ($school in $school)
+{
 New-AddressList -Name $school   -ConditionalCustomAttribute10 $school -IncludedRecipients "AllRecipients"
 $addresslist = (Get-AddressList $school).name+' GAL'
 New-GlobalAddressList -Name "$addresslist" -ConditionalCustomAttribute10  $school -IncludedRecipients "AllRecipients"
@@ -13,3 +30,10 @@ New-OfflineAddressBook -Name $school -AddressLists "\$gal"
 New-AddressBookPolicy -Name $school -AddressLists "\$school" -RoomList "\All Rooms" -OfflineAddressBook "$school" -GlobalAddressList "$gal"
 Get-Mailbox | where{$_.customattribute10 -like "*$school*"} | Set-Mailbox -AddressBookPolicy $school
 }
+}
+catch{
+$_.Exception.Message | out-file -Filepath $logfile -append
+}
+$end = [system.datetime]::Now
+$resultTime = $end - $start
+Write-Host "Execution took : $($resultTime.TotalSeconds) seconds." -ForegroundColor Cyan
