0fd1c3ef9dc2f152dabafa6d7f8965cbd56272f5
Doli Geetha Sai Kumari <62688828+Geetha63@users.noreply.github.com>
2021-02-08 15:53:46 +0530

    Update ABP-UpdateUsersAttributes.ps1

--- Address book policy-UpdateUserAttributes-EducationDomain/ABP-UpdateUsersAttributes.ps1
+++ Address book policy-UpdateUserAttributes-EducationDomain/ABP-UpdateUsersAttributes.ps1
@@ -1,18 +1,53 @@
 ﻿#Setting custom Attribute for students  
+$logfile = "C:\log_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+$start = [system.datetime]::Now
 
+If(Get-Module -ListAvailable -Name AzureAD) 
+ { 
+ Write-Host "AzureAD Already Installed" 
+ } 
+ else { 
+ try { Install-Module -Name AzureAD
+ Write-Host "Installed AzureAD"
+ }
+ catch{
+        $_.Exception.Message | out-file -Filepath $logfile -append
+ }
+ If(Get-Module -ListAvailable -Name MicrosoftTeams) 
+ { 
+ Write-Host "MicrosoftTeams Already Installed" 
+ } 
+ else { 
+ try { Install-Module -Name MicrosoftTeams
+ Write-Host "Installed MicrosoftTeams"
+ }
+ catch{
+        $_.Exception.Message | out-file -Filepath $logfile -append
+ }
+ 
+ try{
 $credential= get-credential
 Connect-AzureAD -Credential $credential
 Connect-ExchangeOnline -credential $credential
+}
+catch{
+        $_.Exception.Message | out-file -Filepath $logfile -append
+ }
+ #Setting custom Attribute for Students
+ 
 
 $group = Get-AzureADGroup -SearchString "Students of" -All $true
-
 foreach ($group in $group){
 $school= $group.displayname
 $school = $school -replace("students of","")
 $school=$school.Where({ $_ -ne "" })
 $school=$school.Trim()
+try{
 $group | Get-AzureADGroupMember | Get-EXOMailbox | Set-Mailbox -CustomAttribute10 $school
-}
+}catch{
+        $_.Exception.Message | out-file -Filepath $logfile -append
+ }}
+
 
 #Setting custom Attribute for teachers
 $group = Get-AzureADGroup -SearchString "teachers of" -All $true
@@ -21,5 +56,12 @@ $school= $group.displayname
 $school = $school -replace("teachers of","")
 $school=$school.Where({ $_ -ne "" })
 $school=$school.Trim()
+try{
 $group | Get-AzureADGroupMember | Get-EXOMailbox | Set-Mailbox -CustomAttribute10 $school
 }
+catch{
+        $_.Exception.Message | out-file -Filepath $logfile -append
+ }}
+$end = [system.datetime]::Now
+$resultTime = $end - $start
+Write-Host "Execution took : $($resultTime.TotalSeconds) seconds." -ForegroundColor Cyan
