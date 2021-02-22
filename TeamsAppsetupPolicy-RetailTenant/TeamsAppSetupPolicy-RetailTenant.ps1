e1b84d2444cc989ed29e0a055c336465ae0947d7
Doli Geetha Sai Kumari <62688828+Geetha63@users.noreply.github.com>
2021-02-22 17:38:07 +0530

    Create TeamsAppSetupPolicy-RetailTenant.ps1

---
+++ TeamsAppsetupPolicy-RetailTenant/TeamsAppSetupPolicy-RetailTenant.ps1
@@ -0,0 +1,19 @@
+﻿$logfile = "C:\TeamsAppSetupPolicylog_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+$start = [system.datetime]::Now
+Import-Module SkypeOnlineConnector
+$sfbSession = New-CsOnlineSession 
+Import-PSSession $sfbSession -AllowClobber
+
+$UserPrincipalNames=Import-Csv AppSetupPolicyInput.csv
+Get-CSTeamsAppsetuppolicy |fl Identity
+$PolicyName=Read-Host "Please provide the PolicyName"
+foreach($UserPrincipalName in $UserPrincipalNames.UserPrincipalName)
+{
+    try{
+    Get-CsOnlineUser -Identity "$UserPrincipalName" | fl TeamsAppSetupPolicy 
+    Grant-CsTeamsAppSetupPolicy -identity "$UserPrincipalName" -PolicyName  "$PolicyName"
+    }
+    catch{
+          $_.Exception.Message | out-file -Filepath $logfile -append
+    }
+}
