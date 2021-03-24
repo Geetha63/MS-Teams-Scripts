11cd3f08432170ef0b0b50a200687bbd8bf14f44
Doli Geetha Sai Kumari <62688828+Geetha63@users.noreply.github.com>
2021-03-24 15:23:13 +0530

    Update GuestRestricationForGivenTeams.ps1

--- GuestRestrictionForGivenTeams/GuestRestricationForGivenTeams.ps1
+++ GuestRestrictionForGivenTeams/GuestRestricationForGivenTeams.ps1
@@ -1,12 +1,12 @@
-﻿#this script will take the input from current folder and create output in current folder (input.csv should have Teamid, AllowToAddGuests) 
+﻿#this script will take the input from current folder and create output in current folder (Teamslist.csv should have Teamid, AllowToAddGuests) 
 #this script will change the Given Teams groups AllowToAddGuests value to False
 
-param(
-      [Parameter(Mandatory=$true)][System.String]$client_Id,
-      [Parameter(Mandatory=$true)][System.String]$Client_Secret,
-      [Parameter(Mandatory=$true)][System.String]$Tenantid
-     )
+$logfile = "C:\log_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+$start = [system.datetime]::Now
 
+     $Tenantid=read-host "Please provide tenant id"
+     $client_Id=Read-host "Please provide client id"
+     $Client_Secret=read-host "Please provide client secret"
 
 #Grant Adminconsent 
 $Grant= 'https://login.microsoftonline.com/common/adminconsent?client_id='
@@ -28,14 +28,18 @@ if ($proceed -eq 'Y')
     } 
 
     $loginurl = "https://login.microsoftonline.com/" + "$Tenantid" + "/oauth2/v2.0/token"
+    try{
     $Token = Invoke-RestMethod -Uri "$loginurl" -Method POST -Body $ReqTokenBody -ContentType "application/x-www-form-urlencoded"
-
+      }
+      Catch {
+    $_.Exception | Out-File $logfile -Append
+   }
     $Header = @{
         Authorization = "$($token.token_type) $($token.access_token)"
     }
 
  #Getting Team details
-    $TeamsList = Import-Csv "input.csv"
+    $TeamsList = Import-Csv "Teamslist.csv"
     foreach($Teams in $TeamsList){
   
                         $uri = "https://graph.microsoft.com/v1.0/groups/" + $Teams.Teamsid
@@ -43,7 +47,12 @@ if ($proceed -eq 'Y')
                         
                         #Get group settings
                         $settingsuri = "https://graph.microsoft.com/v1.0/groups/" + $Teams.Teamsid + "/settings"
+                        try{
                         $groupsettings = Invoke-RestMethod -Headers $Header -Uri $settingsuri -Method get -ContentType 'application/json'
+                        } 
+                        Catch {
+                            $_.Exception | Out-File $logfile -Append
+                           }
                         $Groupvalue = $groupsettings.value
         $body = '{
   "displayName": "Group.Unified.Guest",
@@ -108,18 +117,33 @@ if ($proceed -eq 'Y')
                                                #if settings available update the settings
                                                $settingsid = $Groupvalue.id
                                                $patchuri = "https://graph.microsoft.com/v1.0/groups/" +$Teams.Teamsid+ "/settings/" + "$settingsid" 
+                                               try{
                                                $updatesettings = Invoke-RestMethod -Headers $Header -Uri $patchuri -Method Patch -Body $body -ContentType 'application/json'
+                                               }
+                                               Catch {
+                                                    $_.Exception | Out-File $logfile -Append
+                                                   }
                                                write-host "settings has been updated for" $details.displayName
                                               }
                                         else{ 
                                               #create settings template and apply
                                               $createuri = "https://graph.microsoft.com/v1.0/groups/" +$Teams.Teamsid+ "/settings/"
+                                              try{
                                               $newsettings = Invoke-RestMethod -Headers $Header -Uri $createuri -Method Post -Body $body -ContentType 'application/json'
+                                              }
+                                              Catch {
+                                                    $_.Exception | Out-File $logfile -Append
+                                                   }
                                               write-host "new settings has been created and applied to Team" $details.displayName
                                               }
 
                       $finaluri = "https://graph.microsoft.com/v1.0/groups/" +$Teams.Teamsid+ "/settings" 
+                      try{
                       $final = Invoke-RestMethod -Headers $Header -Uri $finaluri -Method Get -ContentType 'application/json'
+                      }
+                      Catch {
+                                  $_.Exception | Out-File $logfile -Append
+                                 }
                       $status = $final.value.values.value
                       
                       $Teamuri = "https://graph.microsoft.com/v1.0/groups/" + $Teams.Teamsid
@@ -137,3 +161,6 @@ if ($proceed -eq 'Y')
 {
     write-host "You need to login admin consent in order to continue... " 
 }
+$end = [system.datetime]::Now
+$resultTime = $end - $start
+Write-Host "Execution took : $($resultTime.TotalSeconds) seconds." -ForegroundColor Cyan    
