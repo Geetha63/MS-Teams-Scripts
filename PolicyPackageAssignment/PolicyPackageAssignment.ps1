dfe04443e366fed10aab659300bed2945d8e2b73
Doli Geetha Sai Kumari <62688828+Geetha63@users.noreply.github.com>
2021-03-26 13:04:36 +0530

    Update PolicyPackageAssignment.ps1

--- PolicyPackageAssignment/PolicyPackageAssignment.ps1
+++ PolicyPackageAssignment/PolicyPackageAssignment.ps1
@@ -1,11 +1,9 @@
-﻿#this script will take the input from current folder and create output in current folder  (keep the info.json file in same folder where you are running the script)
-#this script will take the input as objectid
+$logfile = "C:\log_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+$start = [system.datetime]::Now
 
-param(    
-      [Parameter(Mandatory=$true)][System.String]$client_Id,
-      [Parameter(Mandatory=$true)][System.String]$Client_Secret,
-      [Parameter(Mandatory=$true)][System.String]$Tenantid     
-      ) 
+     $Tenantid=read-host "Please provide tenant id"
+     $client_Id=Read-host "Please provide client id"
+     $Client_Secret=read-host "Please provide client secret"
 
 #connect to teams
 Connect-MicrosoftTeams
@@ -30,23 +28,33 @@ if ($proceed -eq 'Y')
     } 
 
     $loginurl = "https://login.microsoftonline.com/" + "$Tenantid" + "/oauth2/v2.0/token"
+    try{
     $Token = Invoke-RestMethod -Uri "$loginurl" -Method POST -Body $ReqTokenBody -ContentType "application/x-www-form-urlencoded"
-
+      }
+      Catch {
+          $_.Exception | Out-File $logfile -Append
+         }
     $Header = @{
         Authorization = "$($token.token_type) $($token.access_token)"
     }
 
     $uri = "https://graph.microsoft.com/v1.0/users"
+    try{
     $group = Invoke-RestMethod -Headers $Header -Uri $uri  -Method Get
-
-    
-
+      }
+      Catch {
+    $_.Exception | Out-File $logfile -Append
+   }
     do
     { 
         foreach($value in $group.value)
        { 
+       try{
        $Token1 = Invoke-RestMethod -Uri "$loginurl" -Method POST -Body $ReqTokenBody -ContentType "application/x-www-form-urlencoded"
-
+            }
+            Catch {
+                $_.Exception | Out-File $logfile -Append
+               }
     $Header1 = @{
         Authorization = "$($token1.token_type) $($token1.access_token)"
     }
@@ -56,7 +64,12 @@ if ($proceed -eq 'Y')
 
     #Check if user is assigned any license
         $licenseuri = "https://graph.microsoft.com/v1.0/users/" + "$id" + "/licenseDetails"
+        try{
         $licenseresult = Invoke-RestMethod -Headers $Header1 -Uri $licenseuri  -Method Get
+        }
+        Catch {
+                $_.Exception | Out-File $logfile -Append
+               }
         $licensevalue = $licenseresult.value
         $skuids = $licensevalue.skuId
         $licenses = $licensevalue.skuPartNumber
@@ -66,43 +79,53 @@ if ($proceed -eq 'Y')
         $provisioningStatus = $TeamslicenseStatus.provisioningStatus
         
         $useruri = "https://graph.microsoft.com/v1.0/users/" + $id
+        try{
         $userresult = Invoke-RestMethod -Headers $Header1 -Uri $useruri  -Method Get
-                
+          }
+          Catch {
+                $_.Exception | Out-File $logfile -Append
+               }
          if($licenses -contains "M365EDU_A5_FACULTY")
          {
+         try{
          Grant-CsUserPolicyPackage -Identity $UPN -PackageName "Education_Teacher"
+         }
+         Catch {
+                $_.Exception | Out-File $logfile -Append
+               }
          write-host " Education_Teacher policy has been assigned to user" $UPN
          }
          elseif($licenses -contains "M365EDU_A5_STUDENT")
          {
+         try{
          Grant-CsUserPolicyPackage -Identity $UPN -PackageName "Education_SecondaryStudent"
+         }
+         Catch {
+                $_.Exception | Out-File $logfile -Append
+               }
          write-host " Education_SecondaryStudent policy has been assigned to user" $UPN
          }
 
          else{
-
          Write-Host "User have the diffrent license" $UPN
             $file = New-Object psobject
             $file | add-member -MemberType NoteProperty -Name UserName $UPN
             $file | add-member -MemberType NoteProperty -Name Userid $id
             $file | export-csv license.csv -NoTypeInformation -Append
-
-
          }
-                        
-                              
-        
-        }
-        
- 
-if ($group.'@odata.nextLink' -eq $null ) 
+          }
+   if ($group.'@odata.nextLink' -eq $null ) 
         { 
         break 
         } 
         else 
         {
+        try{
         $Token2 = Invoke-RestMethod -Uri "$loginurl" -Method POST -Body $ReqTokenBody -ContentType "application/x-www-form-urlencoded"
-
+            }
+            Catch {
+                $_.Exception | Out-File $logfile -Append
+               }
     $Header2 = @{
         Authorization = "$($token2.token_type) $($token2.access_token)"
     }
@@ -116,3 +139,6 @@ if ($group.'@odata.nextLink' -eq $null )
 {
     write-host "You need to login admin consent in order to continue... " 
 }
+$end = [system.datetime]::Now
+$resultTime = $end - $start
+Write-Host "Execution took : $($resultTime.TotalSeconds) seconds." -ForegroundColor Cyan
