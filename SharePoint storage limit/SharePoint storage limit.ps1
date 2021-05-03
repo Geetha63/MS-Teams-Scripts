c2fc8e8ae767a9bfb5916048939dd3bebcc4e3f0
Doli Geetha Sai Kumari <62688828+Geetha63@users.noreply.github.com>
2021-05-03 18:14:23 +0530

    Update SharePoint storage limit.ps1

--- SharePoint storage limit/SharePoint storage limit.ps1
+++ SharePoint storage limit/SharePoint storage limit.ps1
@@ -1,12 +1,20 @@
-$logfile = "C:\SharePointstoragelimitlog_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
+#This script will restrict the Teams storage. 
+#"Please provide SiteName ex:https://contoso.sharepoint.com/sites/HrTeam"
+# "Please provide StorageQuota"
+# "Please provide StorageQuotaWarningLevel"  
+
+param(
+      [Parameter(Mandatory=$true)][System.String]$SiteName,
+      [Parameter(Mandatory=$true)][System.String]$StorageQuota,
+      [Parameter(Mandatory=$true)][System.String]$StorageQuotaWarningLevel
+      )
+
+$logfile = ".\SharePointstoragelimitlog_$(get-date -format `"yyyyMMdd_hhmmsstt`").txt"
 $start = [system.datetime]::Now
 # site name = https://contoso.sharepoint.com/sites/HrTeam
 # StorageQuota = 20000 (input is in GB)
 # StorageQuotaWarningLevel = 19000 (input is in GB)]
-
-$SiteName=Read-Host "Please provide SiteName ex:https://contoso.sharepoint.com/sites/HrTeam"
-$StorageQuota=Read-Host "Please provide StorageQuota"
-$StorageQuotaWarningLevel =Read-Host "Please provide StorageQuotaWarningLevel"        
+      
 try{
 Set-SPOSite -Identity $SiteName -StorageQuota $StorageQuota -StorageQuotaWarningLevel $StorageQuotaWarningLevel -NoWait
 }
